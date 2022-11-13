f53375b6f5e0977bf42ad6f377e5fd61575152eb
Pruthviraj <pruthviraj.p@somaiya.edu>
2022-11-14 00:12:36 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# Auction-site
-This is an auction site
+# Auction Website
+
+This repo contains the code to host an auction website for free using GitHub Pages and Firebase anonymous authentication & databases.
+
+This is a project I worked on for a charity as a pet project and so the functionality is very bespoke and not perfect (as much as I tried to make it). If you would like to contribute to making this codebase more general I welcome you to create pull requests with your improvements.
+
+## Table of contents
+- [Table of contents](#table-of-contents)
+- [Functionality](#functionality)
+- [Setup](#setup)
+  - [Adding auction information](#adding-auction-information)
+  - [Firebase setup](#firebase-setup)
+    - [Creating a project](#creating-a-project)
+    - [Authentication](#authentication)
+    - [Database](#database)
+  - [Creating an admin account and initialising your auctions](#creating-an-admin-account-and-initialising-your-auctions)
+
+## Functionality
+
+- Device based login requiring only a username to be provided (no need to store sensitive information).
+- Popups for more detailed descriptions and additional imagery.
+- Realtime bidding using event listeners (no need to refresh page).
+
+![](./readme/homepage_desktop.png) ![](./readme/loginpage.png)
+
+![](./readme/infopage.png) ![](./readme/bidpage.png)
+
+It looks great on mobile too!
+
+![](./readme/homepage_mobile.png) 
+
+## Setup
+Here we will cover how to add your own information to the auctions themselves, then how to most a local server to see your changes and finally how to connect it all to Firebase to enable user login and bidding.
+
+### Adding auction information
+First, set `demoAuction=False` (this will keep the cats at bay).
+
+Then, populate all of the arrays at the top of `js/auctions.js` with the information for of the items you'll be putting up for auction.
+
+The only complicated option is `endTimes`, which is a list of times (relative to epoch) **in milliseconds**. See [JavaScript's `Date` class](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date) for more information.
+
+### Firebase setup
+Here we will cover how to set up your Firebase project and then how to enable the Firebase authentication and database respectively.
+
+#### Creating a project
+You can create a project using the following steps:
+- Head to the [Firebase website](https://console.firebase.google.com/) where you can manage your projects.
+- Click `Add project` and choose your project's name.
+- Then you may enable or disable Google Analytics at your discretion.
+- You will then be taken to your project's console (https://console.firebase.google.com/project/your-project-name/overview) where you will add a web app to your project by clicking on the relevant button and chosing a name for the app (this is arbitrary and only to identify your app within the project if, for example, you had multiple apps).
+- Now that you have created an app you should be shown a code snipped containing `firebaseConfig`. Copy everything inside `firebaseConfig` to the `firebaseConfig` variable in `index.html`.
+- Finally you will want to update the version of the Firebase SDKs that are imported after [line 33 of `index.html`](https://github.com/HMellor/auction-website/blob/master/index.html#L33). The most recent version will be shown in the script tag above `firebaseConfig` when you create your project.
+
+(The code snippet containing `firebaseConfig` and the current version can be found in general project settings if you missed it earlier)
+
+#### Authentication
+Setting up authentication is very simple. Head to your project's console and click on Authentication in the menu on the left. Then go to the Sign-in method tab and enable toggle switch for the Anonymous sign-in provider.
+
+#### Database
+Setting up the database is a little more involved so here are the steps you must take:
+- Head to your project's console and click on Database in the menu on the left. Then click on `Create database` (the mode you start in does not matter because we are about to set proper rules anyway).
+- Then chose which region you want your Firestore to be stored (check the server locations [here](https://firebase.google.com/docs/firestore/locations) if there are multiple in your region).
+- Head to the `Rules` tab and paste the rules below.
+```
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+  	function isAdmin() {
+    	return get(/databases/$(database)/documents/users/$(request.auth.uid)).data.admin == "insert long random secret string"
+    }
+    function isDocumentOverWrite() {
+    	return request.resource.data.keys().hasOnly(resource.data.keys())
+    }
+    function isFieldOverWrite() {
+    	return request.resource.data[request.resource.data.keys()[0]].keys().hasOnly(resource.data[request.resource.data.keys()[0]].keys())
+    }
+    function isLoggedIn() {
+    	return exists(/databases/$(database)/documents/users/$(request.auth.uid))
+    }
+  	match /users/{user} {
+    	allow read, update, delete: if false;
+    	allow create: if true;
+    }
+    match /auction-live/{items} {
+      allow get, list: if true;
+    	allow create, delete: if isAdmin();
+      allow update: if isAdmin() || isLoggedIn(); //&& !isFieldOverWrite();
+    }
+    match /auction-store/{item} {
+      allow get, list: if false;
+    	allow create, delete: if isAdmin();
+      allow update: if isAdmin() || isLoggedIn() && !isDocumentOverWrite();
+    }
+  }
+}
+```
+- These rules state that:
+  - Users (admin or otherwise) can only create user documents, ensuring that no one but you can see the users names (a privacy measure).
+  - The auction live  document may be read by anyone and only updated if the user is logged in (or if the user is an admin). This document is what your clients will fetch the current state of the auction from. Therefore no real names are stored here, only the bid amount and the user's UID (which is randomly generated by Firebase and is completely non-identifying to any prying eyes).
+  - The auction store documents may not be read by anyone and only be written to if the user is logged in and the requested write would not overwrite any previous bids (or if the user is an admin). These documents serve as both a backup copy of the auction that cannot be meddled with and a directory of bid information containing user names for your eyes only.
+- You may notice that the `isFieldOverWrite()` call for the `auction-live` rule is commented out. This is because it only works for a single auction and thus, in its current state, would be fatal to implement in the auction. If you are able to make it work correctly I would appreciate it if you let me know your solution either in a pull request or in a comment on the [relevant issue](https://github.com/HMellor/auction-website/issues/5).
+
+### Creating an admin account and initialising your auctions
+The final step in setting up your auction is to create an admin account and use it to initialise your auctions.
+
+To create an admin account:
+- Host your website either locally or on GitHub Pages and log in to your website.
+- Then go to your Firestore console and find the document for the user you just created.
+- There should be a field in the document called `admin` which has the value `"false"`. You must now create a password (or hash) that enables admin access and set the `admin` field to this value.
+- With your admin password chosen, go to your Firestore rules and replace `"insert long random secret string"` with it.
+- You have now created your admin account.
+
+To initialise the auctions:
+- With the device you used to create your admin account, head to your website.
+- Open the developer console (F12) and type the following into the console at the bottom:
+```
+resetAll()
+```
+- This will wipe all documents in the `auction-live` and `auction-store` collections and create auctions with the titles, info and reserve prices you defined earlier (as long as you are admin).
+- Your auction is now ready for your event.
---
+++ js/auctions.js
@@ -0,0 +1,352 @@
+// For a real auction, set this to false
+let demoAuction = true;
+// For a real auction, populate these arrays
+let primaryImages = [];
+let titles = [];
+let subtitles = [];
+let details = [];
+let secondaryImages = [];
+let startingPrices = [55, 60, 20, 0, 4, 0, 99, 0, 12, 6, 3, 7];
+let endTimes = []; // Make sure to fix these to UTC time so they don't change with the users timezone
+
+// Random auction information
+function generateRandomAuctions() {
+  // Random cat images
+  document.querySelectorAll(".card > img").forEach(img => {
+    img.src = "https://source.unsplash.com/random/900×700/?car"
+    primaryImages.push(img.src);
+    secondaryImages.push(img.src);
+  });
+  // Random cat names
+  $.getJSON(
+    "https://random-data-api.com/api/name/random_name",
+    { size: startingPrices.length },
+    function (data) {
+      data.forEach((elem, idx) => {
+        document.querySelector("#auction-" + idx + " > div > h5").innerHTML = elem.name;
+        titles.push(elem.name);
+      });
+    }
+  );
+  // Random lorem ipsum cat descriptions
+  $.getJSON(
+    "https://random-data-api.com/api/lorem_ipsum/random_lorem_ipsum",
+    { size: startingPrices.length },
+    function (data) {
+      data.forEach((elem, idx) => {
+        document.querySelector("#auction-" + idx + " > div > p").innerHTML = elem.short_sentence;
+        subtitles.push(elem.short_sentence);
+        details.push(elem.very_long_sentence);
+      });
+    }
+  );
+}
+
+// Initial state of auction, used for resetting database
+let startPrices = [];
+for (let i = 0; i < startingPrices.length; i++) {
+  if (demoAuction) {
+    let now = new Date();
+    let endTime = new Date().setHours(8 + i, 0, 0, 0)
+    if (endTime - now < 0) { endTime = new Date(endTime).setDate(now.getDate() + 1) }
+    endTimes.push(endTime)
+  }
+  startPrices.push({
+    bid0: {
+      bidder: String(i),
+      amount: startingPrices[i],
+      time: Date().substring(0, 24)
+    }
+  })
+}
+
+// Convert time to string for HTML clocks
+function timeBetween(start, end) {
+  let _string = ""
+  let secsRemaining = (end - start) / 1000;
+  let d = parseInt(secsRemaining / 86400);
+  let h = parseInt(secsRemaining % 86400 / 3600);
+  let m = parseInt(secsRemaining % 3600 / 60);
+  let s = parseInt(secsRemaining % 60);
+  if (d) { _string = _string + d + "d " }
+  if (h) { _string = _string + h + "h " }
+  if (m) { _string = _string + m + "m " }
+  if (s) { _string = _string + s + "s " }
+  return _string.trim()
+}
+
+// Set time on HTML clocks
+function setClocks() {
+  let now = new Date();
+  let nowTime = now.getTime();
+  for (i = 0; i < startingPrices.length; i++) {
+    let timer = document.getElementById("time-left-" + i)
+    // remove finished auction after 5 minutes
+    if (endTimes[i] - nowTime < -300) {
+      document.getElementById("auction-" + i).parentElement.style.display = "none"
+      if (demoAuction) {
+        endTimes[i] = new Date(endTimes[i]).setDate(now.getDate() + 1) // add 1 day
+        document.getElementById("auction-" + i).parentElement.remove()
+        resetLive(i);
+        resetStore(i);
+        auctionGrid = document.getElementById("auction-grid");
+        auctionCard = generateAuctionCard(i);
+        auctionGrid.appendChild(auctionCard);
+      }
+      // disable bidding on finished auctions
+    } else if (endTimes[i] - nowTime < 0) {
+      timer.innerHTML = "Auction Complete";
+      document.getElementById("bid-button-" + i).setAttribute('disabled', '')
+    } else {
+      timer.innerHTML = timeBetween(nowTime, endTimes[i]);
+    }
+  }
+  setTimeout(setClocks, 1000);
+}
+
+// Place a bid on an item
+function placeBid() {
+  let nowTime = new Date().getTime();
+  let modalBidButton = document.querySelector("#bid-modal > div > div > div.modal-footer > button.btn.btn-primary")
+  modalBidButton.setAttribute('disabled', '') // disable the button while we check
+  let i = modalBidButton.id.match("[0-9]+");
+  let feedback = document.getElementById("bad-amount-feedback")
+  // Cleanse input
+  let amountElement = document.getElementById("amount-input")
+  let amount = Number(amountElement.value)
+  if (endTimes[i] - nowTime < 0) {
+    feedback.innerText = "The auction is already over!"
+    amountElement.classList.add("is-invalid")
+    setTimeout(() => {
+      bidModal.hide();
+      amountElement.classList.remove("is-invalid");
+      modalBidButton.removeAttribute('disabled', '');
+    }, 1000);
+  } else if (amount == 0) {
+    // amount was empty
+    feedback.innerText = "Please specify an amount!"
+    amountElement.classList.add("is-invalid")
+    modalBidButton.removeAttribute('disabled', '');
+  } else if (!(/^-?\d*\.?\d{0,2}$/.test(amount))) {
+    // field is does not contain money
+    feedback.innerText = "Please specify a valid amount!"
+    amountElement.classList.add("is-invalid")
+    modalBidButton.removeAttribute('disabled', '');
+  } else {
+    // Checking bid amount
+    // Get item and user info
+    let user = auth.currentUser;
+    let itemId = i.toString().padStart(5, "0")
+    // Documents to check and write to
+    let liveRef = db.collection("auction-live").doc("items")
+    let storeRef = db.collection("auction-store").doc(itemId)
+    // Check live document
+    liveRef.get().then(function (doc) {
+      console.log("Database read from placeBid()")
+      let thisItem = doc.data()[itemId];
+      let bids = (Object.keys(thisItem).length - 1) / 2
+      let currentBid = thisItem["bid" + bids]
+      if (amount >= 1 + currentBid) {
+        keyStem = itemId + ".bid" + (bids + 1)
+        liveRef.update({
+          [keyStem + "-uid"]: user.uid,
+          [keyStem]: amount,
+        })
+        console.log("Database write from placeBid()")
+        storeKey = "bid" + (bids + 1)
+        storeRef.update({
+          [storeKey]: {
+            "bidder-username": user.displayName,
+            "bidder-uid": user.uid,
+            "amount": amount,
+            time: Date().substring(0, 24)
+          }
+        })
+        console.log("Database write from placeBid()")
+        amountElement.classList.add("is-valid")
+        amountElement.classList.remove("is-invalid")
+        setTimeout(() => {
+          bidModal.hide();
+          amountElement.classList.remove("is-valid");
+          modalBidButton.removeAttribute('disabled', '');
+        }, 1000);
+      } else {
+        amountElement.classList.add("is-invalid")
+        feedback.innerText = "You must bid at least £" + (currentBid + 1).toFixed(2) + "!"
+        modalBidButton.removeAttribute('disabled', '');
+      }
+    });
+  }
+}
+
+function argsort(array) {
+  const arrayObject = array.map((value, idx) => { return { value, idx }; });
+  arrayObject.sort((a, b) => (a.value - b.value))
+  return arrayObject.map(data => data.idx);;
+}
+
+function generateAuctionCard(i) {
+  // create auction card
+  let col = document.createElement("div");
+  col.classList.add("col");
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.id = "auction-" + i
+  col.appendChild(card);
+
+  let image = document.createElement("img");
+  image.classList.add("card-img-top");
+  image.src = primaryImages[i];
+  card.appendChild(image);
+
+  let body = document.createElement("div");
+  body.classList.add("card-body");
+  card.appendChild(body);
+
+  let title = document.createElement("h5");
+  title.classList.add("title");
+  title.innerText = titles[i];
+  body.appendChild(title);
+
+  let subtitle = document.createElement("p");
+  subtitle.classList.add("card-subtitle");
+  subtitle.innerText = subtitles[i];
+  body.appendChild(subtitle);
+
+  // Auction status
+  let statusTable = document.createElement("table");
+  statusTable.classList.add("table");
+  card.appendChild(statusTable);
+
+  let tableBody = document.createElement("tbody");
+  statusTable.appendChild(tableBody);
+
+  let bidRow = document.createElement("tr");
+  tableBody.appendChild(bidRow);
+
+  let bidTitle = document.createElement("th");
+  bidTitle.innerHTML = "Current bid:"
+  bidTitle.scope = "row";
+  bidRow.appendChild(bidTitle);
+
+  let bid = document.createElement("td");
+  bid.innerHTML = "£-.-- [- bids]"
+  bid.id = "current-bid-" + i
+  bidRow.appendChild(bid);
+
+  let timeRow = document.createElement("tr");
+  tableBody.appendChild(timeRow);
+
+  let timeTitle = document.createElement("th");
+  timeTitle.innerHTML = "Time left:"
+  timeTitle.scope = "row";
+  timeRow.appendChild(timeTitle);
+
+  let time = document.createElement("td");
+  time.id = "time-left-" + i
+  timeRow.appendChild(time);
+
+  // Auction actions
+  let buttonGroup = document.createElement("div");
+  buttonGroup.classList.add("btn-group");
+  card.appendChild(buttonGroup)
+
+  let infoButton = document.createElement("button");
+  infoButton.type = "button"
+  infoButton.href = "#";
+  infoButton.classList.add("btn", "btn-secondary")
+  infoButton.innerText = "Info";
+  infoButton.onclick = function () { openInfo(this.id); }
+  infoButton.id = "info-button-" + i
+  buttonGroup.appendChild(infoButton);
+
+  let bidButton = document.createElement("button");
+  bidButton.type = "button"
+  bidButton.href = "#";
+  bidButton.classList.add("btn", "btn-primary")
+  bidButton.innerText = "Submit bid";
+  bidButton.onclick = function () { openBid(this.id); }
+  bidButton.id = "bid-button-" + i
+  buttonGroup.appendChild(bidButton);
+
+  return col
+}
+
+// Generatively populate the websire with auctions
+function populateAuctionGrid() {
+  auctionGrid = document.getElementById("auction-grid");
+  let endingSoonest = argsort(endTimes);
+  endingSoonest.forEach((i) => {
+    auctionCard = generateAuctionCard(i);
+    auctionGrid.appendChild(auctionCard);
+  });
+  if (demoAuction) { generateRandomAuctions() };
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function dataListener() {
+  // Listen for updates in active auctions
+  db.collection("auction-live").doc("items").onSnapshot(function (doc) {
+    console.log("Database read from dataListener()")
+    let data = doc.data()
+    for (key in data) {
+      let cb = document.getElementById("current-bid-" + Number(key))
+      let bids = data[key]
+      // Extract bid data
+      let bidCount = (Object.keys(bids).length - 1) / 2
+      let currPound = Number.parseFloat(bids["bid" + bidCount]).toFixed(2)
+      // Check if the user is winning
+      if (auth.currentUser) {
+        let userWinning = bids["bid" + bidCount + "-user"] == auth.currentUser.uid
+      }
+      // Add bid data to HTML
+      cb.innerHTML = "£" + numberWithCommas(currPound) + " [" + bidCount + " bid" + (bidCount != 1 ? "s" : "") + "]"
+    }
+  })
+}
+
+function resetLive(i) {
+  let docRef = db.collection("auction-live").doc("items");
+  let itemId = i.toString().padStart(5, "0")
+  docRef.update({
+    [itemId]: {
+      bid0: startPrices[i]["bid0"]["amount"],
+    }
+  })
+  console.log("Database write from resetLive()")
+}
+
+function resetAllLive() {
+  console.log("Resetting live tracker")
+  for (i = 0; i < startingPrices.length; i++) {
+    resetLive(i);
+  }
+}
+
+function resetStore(i) {
+  let itemId = i.toString().padStart(5, "0")
+  let docRef = db.collection("auction-store").doc(itemId);
+  docRef.set(startPrices[i])
+  console.log("Database write from resetStore()")
+}
+
+function resetAllStore() {
+  console.log("Resetting auction storage")
+  let batch = db.batch();
+  for (i = 0; i < startingPrices.length; i++) {
+    let itemId = i.toString().padStart(5, "0")
+    let currentItem = db.collection("auction-store").doc(itemId);
+    batch.set(currentItem, startPrices[i])
+  }
+  batch.commit()
+  console.log(startingPrices.length + " database writes from resetAllStore()")
+}
+
+function resetAll() {
+  resetAllLive();
+  resetAllStore();
+}
